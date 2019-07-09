e47130780b8d6810e67f902162233e568cd2e207
kika <malimad98@gmail.com>
2019-07-09 14:36:08 +0100

    finishing UI

--- AffineEncryption/src/Controllers/MainFXMLController.java
+++ AffineEncryption/src/Controllers/MainFXMLController.java
@@ -5,9 +5,16 @@
  */
 package Controllers;
 
+import com.jfoenix.controls.JFXTextField;
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 
 /**
  * FXML Controller class
@@ -16,7 +23,38 @@ import javafx.fxml.Initializable;
  */
 public class MainFXMLController implements Initializable {
 
-    
+        @FXML
+    private AnchorPane parent;
+
+    @FXML
+    private Circle admin_avatar_container;
+
+    @FXML
+    private JFXTextField noteToSendTextField;
+
+    @FXML
+    private ImageView sendNoteImgV;
+
+    @FXML
+    private Label receivedNoteLabel;
+
+    @FXML
+    private Label cryptedNoteLabe;
+
+    @FXML
+    void hideCryptedNote(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sendMsg(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showCryptedNote(MouseEvent event) {
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
